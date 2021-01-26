8dca2502c3ac406cc766d4e8b675fc49620839d0
Steve Djau <steve@redbitdev.com>
2021-01-26 17:05:45 -0500

    first commit

---
+++ pdf-testing/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PDFDownload from './components/pdf-download';
+
+function App() {
+  return (
+    <div className="App">
+      <PDFDownload />
+    </div>
+  );
+}
+
+export default App;
